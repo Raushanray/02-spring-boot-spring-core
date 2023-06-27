770343242d93b0a70fe2f82afc1e2a1b8b2afcea
Raushanray <104117973+Raushanray@users.noreply.github.com>
2023-06-27 23:33:13 +0530

    spring boot with @lazy annotatios

---
+++ 06-lazy-initialization/src/main/java/com/codewithraushan/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.codewithraushan.springcoredemo.rest;
+
+import com.codewithraushan.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define private fields for the dependency
+
+    private Coach myCoach;
+
+    @Autowired
+    public  DemoController(@Qualifier("cricketCoach") Coach theCoach){
+        System.out.println("In Constructor :" + getClass().getSimpleName());
+        myCoach  = theCoach;
+    }
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
