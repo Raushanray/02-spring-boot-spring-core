f650a72b5bfc2c0d3c9690c5f3fed4b418b75c3a
Raushanray <104117973+Raushanray@users.noreply.github.com>
2023-06-27 23:21:22 +0530

    spring boot application work with constructor-injection

---
+++ 01-constructor-injection/src/main/java/com/codewithraushan/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.codewithraushan.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 01-constructor-injection/src/main/java/com/codewithraushan/springcoredemo/DemoController.java
@@ -0,0 +1,25 @@
+package com.codewithraushan.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define private fields for the dependency
+
+    private Coach myCoach;
+
+    //define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach thCoach){
+        myCoach = thCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
