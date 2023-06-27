4fd7cf8dcabd0169e2e636e45b420452a3d33612
Raushanray <104117973+Raushanray@users.noreply.github.com>
2023-06-27 23:29:37 +0530

    spring boot with @qualifiers annotations

---
+++ 04-qualifiers/src/main/java/com/codewithraushan/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.codewithraushan.springcoredemo.rest;
+
+import com.codewithraushan.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define private fields for the dependency
+
+    private Coach myCoach;
+
+    @Autowired
+    public  DemoController(@Qualifier("cricketCoach") Coach theCoach){
+        myCoach  = theCoach;
+    }
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
